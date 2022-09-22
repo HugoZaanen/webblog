7e5750cc5249c2c428d8d64c011ee4bc7bc36ef0
Hugo Zaanen <HugoZaanen@hotmail.com>
2022-09-22 14:24:40 +0200

    First commit

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+let post = [];
 
 app.get('/',function(req, res){
   res.render('home',{
